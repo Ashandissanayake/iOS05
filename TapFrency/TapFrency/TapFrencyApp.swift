1551ffe8d493c80adfaf05ad599dd68254c7c678
student5 <student5@NIBMADMIN05s-iMac.local>
2026-07-17 12:32:52 +0530

    tets

---
+++ TapFrency/TapFrency/TapFrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapFrencyApp.swift
+//  TapFrency
+//
+//  Created by student5 on 2026-06-27.
+//
+
+import SwiftUI
+
+@main
+struct TapFrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios_practical/ios_practical/Models/GameMode.swift
@@ -0,0 +1,7 @@
+//
+//  GameMode.swift
+//  ios_practical
+//
+//  Created by student5 on 2026-07-17.
+//
+
